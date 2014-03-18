8ad33cc91a15aa2ddb39a658b9e3ab5d25030339
15165616852 <15165616852@163.com>
2014-03-18 18:23:21 +0800

    added two pages landing pages and home pages

--- Android/src/com/test/activity/MainActivity.java
+++ Android/src/com/test/activity/MainActivity.java
@@ -1,16 +1,43 @@
 package com.test.activity;
 
-import com.test.android.R;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	ListView list;
+	EditText username;
+	EditText password;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		Button btn = (Button)findViewById(R.id.login_button);
+		username = (EditText)findViewById(R.id.username);
+		password = (EditText)findViewById(R.id.password);
+		btn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String s_username = username.getText().toString();
+				String s_password = password.getText().toString();
+				if("admin".equals(s_username) && "admin".equals(s_password)){
+					Intent intent = new Intent();
+					intent.setClass(MainActivity.this, IndexActivity.class);
+					startActivity(intent);
+				} else {
+					Toast.makeText(getApplicationContext(), R.string.login_error, Toast.LENGTH_LONG).show();
+				}
+			}
+		});
+		list = (ListView)findViewById(R.id.list);
 	}
+	
 }
