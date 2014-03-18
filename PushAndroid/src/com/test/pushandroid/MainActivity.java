9e22c49be0da55617a005ef9e6c16a4cb3a7d0e1
15165616852 <15165616852@163.com>
2014-03-18 13:37:02 +0800

    add
    add words

--- PushAndroid/src/com/test/pushandroid/MainActivity.java
+++ PushAndroid/src/com/test/pushandroid/MainActivity.java
@@ -2,13 +2,16 @@ package com.test.pushandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	EditText text;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		text = (EditText)findViewById(R.id.editText1);
 	}
 }
