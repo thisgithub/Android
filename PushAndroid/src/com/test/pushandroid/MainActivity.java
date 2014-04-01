fe0cc93921f1443e0daa789f43cd28e347ef8842
thisgithub <15165616852@163.com>
2014-04-02 00:02:23 +0800

    add some words of pushandroid

--- PushAndroid/src/com/test/pushandroid/MainActivity.java
+++ PushAndroid/src/com/test/pushandroid/MainActivity.java
@@ -1,17 +1,58 @@
 package com.test.pushandroid;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends Activity {
-	EditText text;
+public class MainActivity extends ActionBarActivity {
+	public static final String EXTRA_MESSAGE = "com.test.pushandroid.MESSAGE";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		text = (EditText)findViewById(R.id.editText1);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_activity_actions, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch(item.getItemId()){
+		case R.id.action_search:
+			openSearch();
+			return true;
+		case R.id.action_settings:
+			openSearch();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	
+	private void openSearch() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void sendMessage(View view){
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText)findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
 	}
 }
